bfa9ac90a4f5ae2584a3a5f36a11ca17e721c0ee
Elizarspbspb <elizarspb@yandex.ru>
2023-08-28 22:52:35 +0300

    5 glava

--- OOP_2004/5.Functions/first.cpp
+++ OOP_2004/5.Functions/first.cpp
@@ -24,6 +24,14 @@ void engldisp(Distance); // объявление
 // демонстрирует механизм возврата значения функцией
 float lbstokg(float);   // прототип функции
 
+// retstrc.cpp
+// демонстрирует возвращение функцией структурной переменной
+Distance addengl(Distance, Distance);   // прототипы
+//////////////////////////////////////////////////////////
+
+// ref.cpp
+// применение ссылочного механизма передачи аргументов
+
 int main()
 {
     // table.cpp
@@ -60,6 +68,35 @@ int main()
     kgs = lbstokg(lbs);
     cout << "Вес в килограммах равен " << kgs << endl;
 
+    starline();
+
+    // retstrc.cpp
+    // демонстрирует возвращение функцией структурной переменной
+    Distance d3, d4, d5;    // три структурные переменные
+    d3.feet = 10;
+    d3.inches = 9;
+    d4.feet = 6;
+    d4.inches = 12;
+    d5 = addengl(d3, d4);   // d3 равно сумме d1 и d2
+    engldisp(d3); 
+    cout << " + ";
+    engldisp(d4); 
+    cout << " = ";
+    engldisp(d5); 
+    cout << endl;
+
+    starline();
+
+    // ref.cpp
+    // применение ссылочного механизма передачи аргументов
+    void intfrac(float, float&, float&);    // прототип
+    float number, intpart, fracpart;
+    number = 10.56;
+    intfrac(number, intpart, fracpart);     // нахождение целой и дробной части
+    cout << "Целая часть равна " << intpart // вывод результатов
+    << ", дробная часть равна " << fracpart << endl;
+
+
     return 0;
 }
 
@@ -87,4 +124,26 @@ void engldisp(Distance dd) // параметр dd типа Distance (КОПИЯ)
 float lbstokg(float pounds) {
     float kilograms = 0.453592 * pounds;
     return kilograms;
+}
+
+// демонстрирует возвращение функцией структурной переменной
+Distance addengl(Distance dd1, Distance dd2)
+{
+    Distance dd3;   // переменная для хранения будущей суммы
+    dd3.inches = dd1.inches + dd2.inches;   // сложение дюймов
+    dd3.feet = 0;   // с заемом;
+    if(dd3.inches >= 12.0) {    // если число дюймов больше 12.0, то уменьшаем число дюймов
+        dd3.inches -= 12.0;     // на 12.0 и увеличиваем
+        dd3.feet++;
+    }
+    dd3.feet += dd1.feet + dd2.feet; // сложение футов
+    return dd3;     // возврат значения
+}
+//--------------------------------------------------------
+
+// функция intfrac() вычисляет целую и дробную часть вещественного числа
+void intfrac(float n, float& intp, float& fracp) {
+    long temp = static_cast<long>(n);   // преобразование к типу long,
+    intp = static_cast<float>(temp);    // и обратно во float
+    fracp = n - intp;                   // вычитаем целую часть
 }
