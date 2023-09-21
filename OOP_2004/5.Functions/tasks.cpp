f8a09878d001f4968e4501c6256ceb2b2a286b0d
user <user@kali.kali>
2023-09-21 16:19:26 +0300

    Finished tasks for 5 chapter

--- OOP_2004/5.Functions/tasks.cpp
+++ OOP_2004/5.Functions/tasks.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 const float PI = 3.14159F;  // вещественная константа
-int count = 0;
 
 // Task 1
 float circarea(float);
@@ -51,7 +50,26 @@ template<typename T>
 void swapT(T&, T&);
 
 // Task 10
+int count = 0;
 void showCount();
+
+// Task 11
+struct sterling {
+    int pounds, shillings, pence;
+};
+sterling getAldMoney(int, int, int);
+sterling sumMoney(sterling&, sterling&);
+void showAldMoney(sterling&);
+
+// Task 12
+struct fraction {
+    int dividend;
+    int divisor;
+};
+fraction fadd(fraction, fraction);
+fraction fsub(fraction, fraction);
+fraction fmul(fraction, fraction);
+fraction fdiv(fraction, fraction);
 
 int main() {
     /*1. Вернитесь к рассмотрению примера CIRCAREA главы 2 «Основы программирования на C++». 
@@ -60,11 +78,11 @@ int main() {
     функцию main(), которая просит пользователя ввести значение радиуса, вызывает функцию circarea(), 
     а затем отображает результат вычисления на экране. */
     // circarea.cpp     работа с переменными вещественного типа
-    /*float rad, area;  // переменная вещественного типа
+    float rad, area;  // переменная вещественного типа
     cout << "Введите радиус окружности: "; // запрос
     cin >> rad;     // получение радиуса
     area = circarea(rad);
-    cout << "Площадь круга равна " << area << endl; */
+    cout << "Площадь круга равна " << area << endl;
 
     /*2. Возведение числа n в степень р — это умножение числа n на себя р раз. Напишите функцию с именем 
     power(), которая в качестве аргументов принимает значение типа double для n и значение типа int для р 
@@ -72,38 +90,38 @@ int main() {
     по умолчанию, равное 2, чтобы при отсутствии показателя степени при вызове функции число n возводилось 
     в квадрат. Напишите функцию main(), которая запрашивает у пользователя ввод аргументов для функции 
     power(), и отобразите на экране результаты ее работы.*/
-    /*double number, result;
+    double number;
     int powerOf;
     cout << "Введите число для возведения в степень: "; 
     cin >> number;  
     cout << "Введите степень для возведения числа: "; 
     cin >> powerOf;
     cout << "Результат операции = " << power(number, powerOf) << endl;
-    //cout << "Результат операции = " << power(number) << endl;*/
+    //cout << "Результат операции = " << power(number) << endl;
 
     /*3. Напишите функцию с именем zeroSmaller(), в которую передаются с помощью ссылок два аргумента 
     типа int, присваивающую меньшему из аргументов нулевое значение. Напишите программу для проверки 
     работы функции. */
-    /*int low = 10;
+    int low = 10;
     int high = 20;
     cout << "low = " << low << "; high = " << high << endl;
     zeroSmaller(low, high);
-    cout << "low = " << low << "; high = " << high << endl;*/
+    cout << "low = " << low << "; high = " << high << endl;
 
     /*4. Напишите функцию, принимающую в качестве аргументов два значения типа Distance и возвращающую 
     значение наибольшего из аргументов (необходимую информацию можно найти в примере RETSTRC). */
-    /*Distance fi, se;
+    Distance fi, se;
     fi.feet = 5;
     fi.inches = 10;
     se.feet = 8;
     se.inches = 12;
-    engldisp(checkDis(fi, se));*/
+    engldisp(checkDis(fi, se));
 
     /*5. Напишите функцию с именем hms_to_secs(), имеющую три аргумента типа int: часы, минуты и секунды. 
     Функция должна возвращать эквивалент переданного ей временного значения в секундах (типа long). 
     Создайте программу, которая будет циклически запрашивать у пользователя ввод значения часов, минут 
     и секунд и выводить результат работы функции на экран. */
-    /*int hours, minutes, seconds;
+    int hours, minutes, seconds;
     long fullSeconds = 0;
     char word, colon;
     while (word != 'n') {
@@ -113,19 +131,19 @@ int main() {
         cout << "Полное время в секундах: " << fullSeconds << endl;
         cout << "Продолжить ввод времени (y or n) : ";
         cin >> word; 
-    }*/
+    }
 
     /*6. Модифицируйте программу, описанную в упражнении 11 главы 4 «Структуры», складывающую два 
     структурных значения типа time. Теперь программа должна включать в себя две функции. Первая, time_to_secs(), 
     принимает в качестве аргумента значение типа time и возвращает эквивалентное значение в секундах типа long. 
     Вторая, secs_to_time(), в качестве аргумента принимает число секунд, имеющее тип long, а возвращает 
     эквивалентное значение типа time. */
-    /*Time total_one, total_two, total_three;
-    char colon;
+    Time total_one, total_two, total_three;
+    char colonT;
     bool good = false;
     while (good != true) {
         cout << "Введите время в формате часы:минуты:секунды (23:41:59): ";
-        cin >> total_one.hours >> colon >> total_one.minutes >> colon >> total_one.seconds;
+        cin >> total_one.hours >> colonT >> total_one.minutes >> colonT >> total_one.seconds;
         if(total_one.hours >= 24 || total_one.hours < 0) {
             cout << "Неправильно введены часы !" << endl;
         } else if(total_one.minutes >= 60 || total_one.minutes < 0) {
@@ -140,7 +158,7 @@ int main() {
     good = false;
     while (good != true) {
         cout << "Введите время в формате часы:минуты:секунды (23:41:59): ";
-        cin >> total_two.hours >> colon >> total_two.minutes >> colon >> total_two.seconds;
+        cin >> total_two.hours >> colonT >> total_two.minutes >> colonT >> total_two.seconds;
         if(total_two.hours >= 24 || total_two.hours < 0) {
             cout << "Неправильно введены часы !" << endl;
         } else if(total_two.minutes >= 60 || total_two.minutes < 0) {
@@ -154,13 +172,13 @@ int main() {
     long total_seсs_two = time_to_secs(total_two);
     total_seсs_two += total_seсs_one;
     total_three = secs_to_time(total_seсs_two);
-    cout << total_three.hours << ":" << total_three.minutes << ":" << total_three.seconds << endl;*/
+    cout << total_three.hours << ":" << total_three.minutes << ":" << total_three.seconds << endl;
 
     /*7. Взяв в качестве основы функцию power() из упражнения 2, работающую только со значением 
     типа double, создайте перегруженные функции с этим же именем, принимающими в качестве аргумента 
     значения типа char, int, long и float. Напишите программу, вызывающую функцию power() со всеми 
     возможными типами аргументов.*/
-    /*char num1 = 'a';
+    char num1 = 'a';
     int num2 = 5;
     long num3 = 3;
     float num4 = 4.5;
@@ -169,21 +187,21 @@ int main() {
     cout << "Результат операции int = " << power(num2, powerOf_1) << endl;
     cout << "Результат операции long = " << power(num3, powerOf_1) << endl;
     cout << "Результат операции float = " << power(num4, powerOf_1) << endl;
-    cout << "Результат операции T = " << powerT(num1, powerOf_1) << endl;*/
+    cout << "Результат операции T = " << powerT(num1, powerOf_1) << endl;
 
     /*8. Напишите функцию с именем swap(), обменивающую значениями два своих аргумента типа int 
     (обратите внимание, что изменяться должны значения переменных из вызывающей программы, а не 
     локальных переменных функции). Выберите способ передачи аргументов. Напишите вызывающую 
     программу main(), использующую данную функцию. */
-    /*int one = 10; 
-    int two = 20;
-    cout << "one = " << one << "; two = " << two << endl;
-    swap(one, two);
-    cout << "one = " << one << "; two = " << two << endl;*/
+    int first = 10; 
+    int second = 20;
+    cout << "one = " << first << "; two = " << second << endl;
+    swap(first, second);
+    cout << "one = " << first << "; two = " << second << endl;
 
     /*9. Переработайте программу из упражнения 8 так, чтобы функция swap() принимала 
     в качестве аргументов значения типа time (см. упражнение 6). */
-    /*Time one, two; 
+    Time one, two; 
     one.hours = 14;
     one.minutes = 30;
     one.seconds = 40;
@@ -194,7 +212,7 @@ int main() {
     cout << two.hours << ":" << two.minutes << ":" << two.seconds << endl;
     swapT(one, two);
     cout << one.hours << ":" << one.minutes << ":" << one.seconds << endl;
-    cout << two.hours << ":" << two.minutes << ":" << two.seconds << endl;*/
+    cout << two.hours << ":" << two.minutes << ":" << two.seconds << endl;
 
     /*10. Напишите функцию, которая при каждом вызове будет выводить на экран количество раз, 
     которое она вызывалась ранее. Напишите программу, которая будет вызывать данную функцию не 
@@ -202,13 +220,61 @@ int main() {
     глобальной переменной и статической локальной переменной для хранения числа вызовов функции. 
     Какой из способов предпочтительней? Почему для решения задачи нельзя использовать обычную 
     локальную переменную?   */
-    /*for(int i=0; i<10; i++) {
+    for(int i=0; i<10; i++) {
         showCount();
-    }*/
+    }
 
-    /*11. */
+    /*11. Напишите программу, использующую структуру sterling, которая описана в упражнении 10 
+    главы 4. Программа должна получать от пользователя значения двух денежных сумм, выраженных в 
+    фунтах, шиллингах и пенсах, складывать эти значения и выводить результат на экран в том же 
+    формате. Необходимо разработать три функции. Первая из них должна получать от пользователя 
+    число фунтов, шиллингов и пенсов и возвращать соответствующее значение типа sterling. Вторая 
+    функция должна принимать в качестве аргументов два значения типа sterling, складывать их и 
+    возвращать значение, также имеющее тип sterling. Третья функция должна принимать аргумент 
+    типа sterling и выводить его значение на экран. */
+    int pounds1, shillings1, pence1, pounds2, shillings2, pence2;
+    sterling oneM, twoM, sumM;
+    cout << "Введите первую сумму £";
+    cin >> pounds1 >> shillings1 >> pence1;
+    cout << "Введите первую сумму £";
+    cin >> pounds2 >> shillings2 >> pence2;
+    oneM = getAldMoney(pounds1, shillings1, pence1);
+    twoM = getAldMoney(pounds2, shillings2, pence2);
+    sumM = sumMoney(oneM, twoM);
+    showAldMoney(sumM);
 
-
+    /* 12. Модифицируйте калькулятор, созданный в упражнении 12 главы 4, так, чтобы каждая 
+    арифметическая операция выполнялась с помощью функции. Функции могут называться fadd(), 
+    fsub(), fmul() и fdiv(). Каждая из функций должна принимать два структурных аргумента 
+    типа fraction и возвращать значение того же типа. */
+    fraction firstOp, secondOp, result; 
+    char fraction, operation, contin = 'y';
+    do {
+        cout << "Введите первую дробь, операцию и вторую дробь: ";
+        cin >> firstOp.dividend >> fraction >> firstOp.divisor >> operation >> secondOp.dividend >> fraction >> secondOp.divisor;
+        switch(operation) {
+            case '+':
+                result = fadd(firstOp, secondOp);
+                cout << result.dividend << "/" << result.divisor << endl;
+                break;
+            case '-':
+                result = fsub(firstOp, secondOp);
+                cout << result.dividend << "/" << result.divisor << endl;
+                break;
+            case '/':
+                result = fdiv(firstOp, secondOp);
+                cout << result.dividend << "/" << result.divisor << endl;
+                break;
+            case '*':
+                result = fmul(firstOp, secondOp);
+                cout << result.dividend << "/" << result.divisor << endl;
+                break;
+            default:
+                cout << fraction << " - некорректная операция" << endl;
+        }
+        cout << "Выполнить еще одну операцию (y/n)? ";
+        cin >> contin;
+    } while(contin != 'n');
     return 0;
 }
 
@@ -346,4 +412,61 @@ void swapT(T& one, T& two) {
 void showCount() {
     static int countStatic = 0;
     cout << count++ << " : " << countStatic++ << endl;
+}
+
+// Task 11
+sterling getAldMoney(int pounds, int shillings, int pence) {
+    sterling money;
+    money.pounds = pounds;
+    money.shillings = shillings;
+    money.pence = pence;
+    return money;
+}
+sterling sumMoney(sterling& one, sterling& two) {
+    sterling sumMoney;
+    sumMoney.pence = one.pence + two.pence;
+    if (sumMoney.pence > 11) {
+        while (sumMoney.pence >= 12) {
+            sumMoney.pence -= 12;
+            two.shillings++;
+        }
+    }
+    sumMoney.shillings = one.shillings + two.shillings;
+    if (sumMoney.shillings > 19) {
+        while (sumMoney.shillings >= 20) {
+            sumMoney.shillings -= 20;
+            two.pounds++;
+        }
+    }
+    sumMoney.pounds = one.pounds + two.pounds;
+    return sumMoney;
+}
+void showAldMoney(sterling& sumMoney) {
+    cout << "Всего £" << sumMoney.pounds << " " << sumMoney.shillings << " " << sumMoney.pence << endl;
+}
+
+// Task 12
+fraction fadd(fraction first, fraction second) {
+    fraction operation;
+    operation.dividend = first.dividend * second.divisor + first.divisor * second.dividend;
+    operation.divisor = first.divisor * second.divisor;            
+    return operation;
+}
+fraction fsub(fraction first, fraction second) {
+    fraction operation;              
+    operation.dividend = first.dividend * second.divisor - first.divisor * second.dividend;
+    operation.divisor = first.divisor * second.divisor;            
+    return operation;
+}
+fraction fmul(fraction first, fraction second) {
+    fraction operation;
+    operation.dividend = first.dividend * second.dividend;
+    operation.divisor = first.divisor * second.divisor;            
+    return operation;
+}
+fraction fdiv(fraction first, fraction second) {
+    fraction operation;
+    operation.dividend = first.dividend * second.divisor;
+    operation.divisor = first.divisor * second.dividend;           
+    return operation;
 }
