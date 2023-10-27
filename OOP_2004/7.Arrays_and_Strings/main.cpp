c9fa7db251a474ea057bea2ec8c4bddbbe7bdb09
user <user@kali.kali>
2023-10-27 17:49:12 +0300

    Continue 7 chapter strings and mass

--- OOP_2004/7.Arrays_and_Strings/main.cpp
+++ OOP_2004/7.Arrays_and_Strings/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 
 using namespace std;
 
 const int DISTRICTS = 4;
 const int MONTHS = 3;
 
+const int MAX = 100;
 //////////////////////////////////////////////////////////////
 
 void display(double[DISTRICTS][MONTHS]);
@@ -52,6 +54,45 @@ public:
 };
 
 ///////////////////////////////////////////////////////////
+// cardaray.cpp     класс игральных карт
+enum Suit { clubs, diamonds, hearts, spades };
+// от 2 до 10 обычные числа
+const int jack = 11;
+const int queen = 12;
+const int king = 13;
+const int ace = 14;
+/////////////////////////////////////////////////////////////
+class card {
+private:
+    int number;
+    Suit suit;
+public:
+    card() {}                   // конструктор
+    void set(int n, Suit s)     // установка значения
+    { suit = s; number = n; }
+    void displayCard();             // показ карты
+};
+/////////////////////////////////////////////////////////////
+void card::displayCard() {
+    if(number >= 2 && number <= 10)
+        cout << number;
+    else {
+        switch(number) {
+            case jack: cout << 'J'; break;
+            case queen: cout << 'Q'; break;
+            case king: cout << 'K'; break;
+            case ace: cout << 'A'; break;
+        }
+    }
+    switch(suit) {
+        case clubs: cout << static_cast<char>(5); break;    // печать в виде символов
+        case diamonds: cout << static_cast<char>(4); break;
+        case hearts: cout << static_cast<char>(3); break;
+        case spades: cout << static_cast<char>(6); break;
+    }
+}
+
+/////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) {
     int age[4];
@@ -175,7 +216,7 @@ int main(int argc, char* argv[]) {
 
     //////////////////////////////////////////////////////////////
 
-    Distance dist[100]; // массив переменных
+    Distance dist[MAX]; // массив переменных
     int n = 0;          // счетчик данных
     char ans;           // ответ пользователя (y/n)
     cout << endl;
@@ -184,16 +225,52 @@ int main(int argc, char* argv[]) {
         dist[n++].getdist();    // получаем и сохраняем длину
         cout << "Продолжить ввод (y/n)?: ";
         cin >> ans;
+        if(n >= MAX) {
+            cout << "\nМассив полон!!!";
+            break;
+        }
     } while(ans != 'n');    // продолжать, пока не будет введено 'n'
     // показываем все введенное
-    for(int j = 0; j < n; j++) {
+    for (int j = 0; j < n; j++) {
         cout << "\nДлина номер " << j + 1 << " : ";
         dist[j].showdist();
     }
     cout << endl;
 
     //////////////////////////////////////////////////////////////
-
+    // cardaray.cpp
+    // класс игральных карт
+    card deck[52];
+    cout << endl;
+    for (int j = 0; j < 52; j++) { // создаем упорядоченную колоду карт
+        int num = (j % 13) + 2;
+        Suit su = Suit(j / 13);
+        deck[j].set(num, su);
+    }
+    // показываем исходную колоду
+    cout << "Исходная колода:\n";
+    for (int j = 0; j < 52; j++) {
+        deck[j].displayCard();
+        cout << " ";
+        if(!((j + 1) % 13))     // начинаем новую строку после каждой 13-й карты
+            cout << endl;
+    }
+    srand(time(NULL));          // инициализируем генератор случайных чисел
+    for (int j = 0; j < 52; j++) {
+        int k = rand() % 52;    // выбираем случайную карту
+        card temp = deck[j];    // и меняем ее с текущей
+        deck[j] = deck[k];
+        deck[k] = temp;
+    }
+    // показываем перемешанную колоду
+    cout << "\nПеремешанная колода:\n";
+    for(int j = 0; j < 52; j++) {
+        deck[j].displayCard();
+        cout << " ";
+        if(!((j + 1) % 13)) // начинаем новую строку после каждой 13-й карты
+            cout << endl;
+    }
+    
     return 0;
 }
 
