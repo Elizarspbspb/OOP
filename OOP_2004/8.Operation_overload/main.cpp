dc46d280aae3459bae9a26b47b9726dd41bb6041
user <user@kali.kali>
2023-11-14 17:38:25 +0300

    Continue study 8 chapter operation overload

--- OOP_2004/8.Operation_overload/main.cpp
+++ OOP_2004/8.Operation_overload/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+#include <string.h>     // для функций strcpy, strcat
+#include <stdlib.h>     // для функции exit
 
 using namespace std;
 
@@ -39,6 +41,38 @@ public:
     }
 };
 
+///////////////////////////////////////////////////////////
+
+// englplus.cpp
+// перегрузка операции + для сложения переменных типа Distances
+class Distance {    // класс английских мер длины
+private:
+    int feet;
+    float inches;
+public:
+    Distance() : feet(0), inches(0.0) { }   // конструктор без параметров
+    Distance(int ft, float in) : feet(ft), inches(in) { }   // конструктор с двумя параметрами
+    void getdist() { // получение информации от пользователя
+        cout << "\nВведите футы: "; cin >> feet;
+        cout << "Введите дюймы: "; cin >> inches;
+    }
+    void showdist() { // показ информации
+        cout << feet << "\'-" << inches << '\"'; 
+    }
+    Distance operator+(Distance) const; // сложение двух длин
+};
+///////////////////////////////////////////////////////////
+// сложение двух длин
+Distance Distance::operator+(Distance d2) const {
+    int f = feet + d2.feet;         // складываем футы
+    float i = inches + d2.inches;   // складываем дюймы
+    if(i >= 12.0) {         // если дюймов стало больше 12
+        i -= 12.0;          // то уменьшаем дюймы на 12
+        f++;                // и увеличиваем футы на 1
+    }
+    return Distance(f, i);  // создаем и возвращаем временную переменную
+}
+
 /////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) {
@@ -65,11 +99,22 @@ int main(int argc, char* argv[]) {
     ctwo2 = ++ctwo1;
     cout << "ctwo1 = " << ctwo1.get_count() << endl;    // снова показываем значения
     cout << "ctwo2 = " << ctwo2.get_count() << endl;
-
     ctwo2 = ctwo1++;
     cout << "ctwo1 = " << ctwo1.get_count();
     cout << "ctwo2 = " << ctwo2.get_count() << endl;
     
+    //////////////////////////////////////////////////////////////
+
+    Distance dist1, dist3, dist4;   // определяем переменные
+    dist1.getdist();                // получаем информацию
+    Distance dist2(11, 6.25);       // определяем переменную с конкретным значением
+    dist3 = dist1 + dist2;          // складываем две переменные
+    dist4 = dist1 + dist2 + dist3;  // складываем несколько переменных
+    cout << "dist1 = "; dist1.showdist(); cout << endl;
+    cout << "dist2 = "; dist2.showdist(); cout << endl;
+    cout << "dist3 = "; dist3.showdist(); cout << endl;
+    cout << "dist4 = "; dist4.showdist(); cout << endl;
+
     //////////////////////////////////////////////////////////////
 
     return 0;
