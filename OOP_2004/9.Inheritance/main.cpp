60488df3d59bb3278a2e79f5faad70e2f48a62d4
Elizarspbspb <elizarspb@yandex.ru>
2023-11-29 22:27:30 +0300

    Started 9 chapter

---
+++ OOP_2004/9.Inheritance/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <string.h>     // для функций strcpy, strcat
+#include <stdlib.h>     // для функции exit
+
+///////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[]) 
+{
+    //////////////////////////////////////////////////////////////
+
+    return 0;
+}
