c02ccbc17e9fffe801bdc0da8c22bfb00db655d5
user <user@kali.kali>
2023-12-07 14:23:29 +0300

    Continue tasks 9 chapter

--- OOP_2004/9.Inheritance/tasks.cpp
+++ OOP_2004/9.Inheritance/tasks.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <ctime>
 #include <string.h>     // для функций strcpy, strcat
+//#include <process.h>    // Task 6 // для функции exit
 
 using namespace std;
 
@@ -83,7 +84,8 @@ public:
 };
 class Pstring : public String {
 public:
-    Pstring(char s[]) { 
+    //Pstring(char s[]) { 
+    Pstring(const char* s) { 
         if (strlen(s) > SZ-1) {
             //strncpy(str, s, SZ-1);
             int j = 0;
@@ -232,6 +234,90 @@ public:
     }
 };
 
+
+// Task 6
+// arrover3.cpp демонстрация создания безопасного массива, проверяющего свои индексы при использовании используется перегрузка операции []
+const int LIMIT = 100;  // размер массива
+class safearray {
+private:
+    int arr[LIMIT];
+public:
+    int& operator[](int n) {  // обратите внимание, что функция возвращает ссылку!
+        if(n < 0 || n >= LIMIT) { cout << "\nОшибочный индекс!"; exit(1); }
+        return arr[n];
+    }
+};
+class safehilo : private safearray {
+    int up;
+    int lower;
+    int dif;
+public:
+    safehilo() = default;
+    safehilo(int u, int l) : up(u), lower(l) { dif = up - lower;}
+    int& operator[](int n) {
+        return safearray::operator[](n - lower);
+    }
+};
+
+// Task 7
+class Counter {
+protected:
+    int count;     // счетчик
+public:
+    Counter() : count() {}
+    Counter(int c) : count(c) { }
+    unsigned int get_count() const // получение значения
+    { return count; }
+    Counter operator++() { return Counter(++count); }
+};
+class CountDn : public Counter {
+public:
+    CountDn() : Counter() { }
+    CountDn(int c) : Counter(c) { }
+    CountDn operator--() { return CountDn(--count); }
+};
+class CountPrefPost : public CountDn {
+public:
+    CountPrefPost() : CountDn() { }
+    CountPrefPost(int c) : CountDn(c) { }
+    // 1 // CountPrefPost& operator++() { ++count; return *this; }
+    // 2 // CountPrefPost operator--() { return CountPrefPost(--count); }
+    // 2 // CountPrefPost operator++() { return CountPrefPost(++count); }
+    CountPrefPost operator--() { CountDn::operator--(); return *this; }
+    CountPrefPost operator++() { CountDn::operator++(); return CountPrefPost(count); }
+    CountPrefPost operator--(int) { return CountPrefPost(--count); }
+    CountPrefPost operator++(int) { return CountPrefPost(++count); }
+};
+
+// Task 8
+class Pstring2 : public Pstring {
+public:
+    Pstring2(char s[]) : Pstring(s) {}
+    //char& left(char s[], int count);
+    char left(char s[], int count);
+    void mid(char s[], int symbol, int count);
+    void right(char s[], int count);
+};
+char Pstring2::left(char s[], int count) {
+    int j = 0;
+    while (j < count) {
+        str[j] = s[j++];
+    }
+    return *str;
+}
+void Pstring2::mid(char s[], int symbol, int count) {
+    int j = symbol;
+    while (j < count+symbol) {
+        str[j] = s[j++];
+    }
+}
+void Pstring2::right(char s[], int count) {
+    int j = count;
+    while (j > 0) {
+        str[j] = s[j--];
+    }
+}
+
 ///////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) 
@@ -298,7 +384,7 @@ int main(int argc, char* argv[])
     scientist2 и laborer2 более соответствовало бы духу ООП. Затем можно применить множественное 
     наследование и сделать так, чтобы эти три новых класса стали производными класса compensation и 
     первоначальных классов manager, scientist и laborer. Таким путем можно избежать модификации исходных классов.*/
-    manager2 man;
+    /*manager2 man;
     man.getdata();
     man.putdata();
     scientist2 sci;
@@ -306,7 +392,66 @@ int main(int argc, char* argv[])
     sci.putdata();
     laborer2 lab;
     lab.getdata();
-    lab.putdata();
+    lab.putdata();*/
+
+    /*6. Вспомним программу ARROVER3 из главы 8. Сохраним класс safearray таким же и, используя наследование, 
+    добавим к нему возможность для пользователя определять верхнюю и нижнюю границы массива в конструкторе.
+    Это похоже на упражнение 9 из главы 8, за исключением того, что применено наследование для создания 
+    нового класса (можно назвать его safehilo) взамен модификации исходного класса. */
+    /*int up, lower;
+    cout << "Введите нижнюю границу массива: ";
+    cin >> lower;
+    cout << "Введите верхнюю границу массива: "; 
+    cin >> up;
+    safehilo sa1(up, lower);
+    int j = 0;
+    for(j = lower; j < up; j++)
+        sa1[j] = j * 10;        // используем функцию слева от знака =
+    for(j = lower; j < up; j++) {
+        int temp = sa1[j];      // используем функцию справа от знака =
+        cout << "Элемент " << j << " равен " << temp << endl;
+    } */
+
+    /*7. Вспомним программу COUNTEN2 из этой главы. В ней можно увеличивать и уменьшать счетчик, используя 
+    префиксные операции. Используя наследование, добавьте возможность использования постфиксных операций 
+    для случаев увеличения и уменьшения. (Описание постфиксных операций вы сможете найти в главе 8.) */
+    /*CountPrefPost c1;     // переменные класса CountPrefPost
+    CountPrefPost c2(100);
+    cout << "\nc1 = " << c1.get_count();        // выводим значения на экран
+    cout << "\nc2 = " << c2.get_count();
+    ++c1; c1++; ++c1;       // увеличиваем c1
+    cout << "\nc1 = " << c1.get_count();        // показываем результат
+    --c2; c2--;
+    cout << "\nc2 = " << c2.get_count();
+    CountPrefPost c3 = c2--;      // создаем переменную c3 на основе c2
+    cout << "\nc3 = " << c3.get_count();        // показываем значение */
+
+    /*8. В некоторых компьютерных языках, таких, как Visual Basic, есть операции, с помощью которых можно 
+    выделить часть строки и присвоить ее другой строке. (В стандартном классе string предложены различные 
+    подходы.) Используя наследование, добавьте такую возможность в класс Pstring из упражнения 2. В новом 
+    производном классе Pstring2 разместите три новых функции: left(), mid() и right().
+        s2.left(s1, n)      // в строку s2 помещаются n самых левых символов строки s1
+        s2.mid(s1, s, n)    // в строку s2 помещаются n символов из строки начиная с символа номер s
+        s2.right(s1, n)     // в строку s2 помещаются n самых правых символов строки s1
+    Вы можете использовать цикл for для копирования символ за символом подходящих частей строки s1 во 
+    временный объект класса Pstring2, который затем их возвратит. Для получения лучшего результата 
+    используйте в этих функциях возврат по ссылке, чтобы они могли быть использованы с левой стороны знака 
+    «равно» для изменения части существующей строки. */
+    /*Pstring2 s1 = "Эта строка имеет очень большую длину и мы можем быть уверены, что она не уместится в отведенный буфер, что приведет к непредсказуемым последствиям.";
+    s1.display();
+    cout << " - - - " << endl;
+    Pstring2 s2 = "Короткая строка.Эта строка имеет очень большую длину и мы можем быть уверены, что она ///////////////////////////////////";
+    s2.left(s1, 5);
+    s2.display(); */
+
+    /*9. Вспомним классы publication, book и type из упражнения 1. Предположим, что мы хотим добавить в 
+    классы book и type дату выхода книги. Создайте новый производный класс publication2, который является 
+    производным класса publication и включает в себя поле, хранящее эту дату. Затем измените классы book и 
+    type так, чтобы они стали производными класса publication2 вместо publication. Сделайте необходимые 
+    изменения функций классов так, чтобы пользователь мог вводить и выводить дату выхода книги. Для даты 
+    можно использовать класс data из упражнения 5 главы 6, который хранит дату в виде трех целых: 
+    для месяца, для дня и для года. */
+
 
     return 0;
 }
