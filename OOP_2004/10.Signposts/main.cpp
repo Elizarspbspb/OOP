2b84b63a09140e57d8561ea58a4a7ceb5489f70e
user <user@kali.kali>
2023-12-18 11:13:56 +0300

    Finished study 10 chapter Signposts

--- OOP_2004/10.Signposts/main.cpp
+++ OOP_2004/10.Signposts/main.cpp
@@ -1,6 +1,8 @@
 // Глава 10. Указатели
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -157,6 +159,170 @@ void bsort(newPerson** pp, int n) {
             order(pp + j, pp + k);  // сортируем два элемента
 }
 
+
+///////////////////////////////////////////////////////////
+// Калькулятор
+
+const int LEN = 80;         // максимальная длина выражения
+const int MAXCALC = 40;
+class Stack {
+private:
+    char st[MAXCALC];       // массив данных
+    int top;                // количество сохраненных данных
+public:
+    Stack() { top = 0; }
+    void push(char var) { st[++top] = var; }    // поместить в стек
+    char pop() { return st[top--]; }            // взять из стека 
+    int gettop() { return top; }                // узнать количество элементов
+};
+class express {
+private:
+    Stack s;        // стек данных
+    char* pStr;     // строка для ввода
+    int len;        // длина строки
+public:
+    express(char* ptr) {
+        pStr = ptr;             // запоминаем указатель на строку
+        len = strlen(pStr);     // устанавливаем длину
+    }
+    void parse();               // разбор выражения
+    int solve();                // получение результата
+};
+void express::parse() { // добавляем данные в стек
+    char ch;            // символ из строки
+    char lastval;       // последнее значение
+    char lastop;        // последний оператор
+    for(int j = 0; j < len; j++) {  // для всех символов в строке
+        ch = pStr[j];   // получаем символ
+        if(ch >= '0' && ch <= '9')  // если это цифра,
+            s.push(ch - '0');       // то сохраняем ее значение
+        else if(ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            if(s.gettop() == 1)     // если это первый оператор,
+                s.push(ch);         // помещаем его в стек
+            else {
+                lastval = s.pop();  // получаем предыдущее число
+                lastop = s.pop();   // получаем предыдущий оператор
+                if((ch == '*' || ch == '/') && (lastop == '+' || lastop == '-')) {  // если это * или /, а предыдущий был + или -, то
+                    s.push(lastop);         // отменяем последние два взятия из стека
+                    s.push(lastval);
+                }
+                else {      // помещаем в стек результат операции
+                    switch(lastop) {
+                        case '+': s.push(s.pop() + lastval); break;
+                        case '-': s.push(s.pop() - lastval); break;
+                        case '*': s.push(s.pop() * lastval); break;
+                        case '/': s.push(s.pop() / lastval); break;
+                        default: cout << "\nНеизвестный оператор"; exit(1);
+                    }
+                }
+                s.push(ch);     // помещаем в стек текущий оператор
+            }
+        } else {    // какая-то ерунда...
+            cout << "\nНеизвестный символ";
+            exit(1);
+        }
+    }
+}
+int express::solve() {      // убираем данные из стека
+    char lastval;           // предыдущее значение
+    while(s.gettop() > 1) {
+        lastval = s.pop();          // получаем предыдущее значение
+        switch(s.pop()) {           // получаем предыдущий оператор
+            case '+': s.push(s.pop() + lastval); break;
+            case '-': s.push(s.pop() - lastval); break;
+            case '*': s.push(s.pop() * lastval); break;
+            case '/': s.push(s.pop() / lastval); break;
+            default: cout << "\nНеизвестный оператор"; exit(1);
+        }
+    }
+    return int(s.pop()); // последний оператор в стеке - это результат
+}
+
+///////////////////////////////////////////////////////////
+// Лошадиные скачки
+const int CPF = 5;
+const int maxHorses = 7;
+class track;
+class horse {
+private:
+    const track* ptrTrack;
+    const int horse_number;
+    float finish_time;
+    float distance_run;
+public:
+    horse(const int n, const track* ptrT) : horse_number(n), ptrTrack(ptrT), distance_run(0.0) { }
+    ~horse() { }
+    void display_horse(const float elapsed_time);
+};
+class track {
+private:
+    horse* hArray[maxHorses];
+    int total_horses;
+    int horse_count;
+    const float track_length;
+    float elapsed_time;
+public:
+    track(float lenT, int nH);
+    ~track();
+    void display_track();
+    void run();
+    float get_track_len() const;
+};
+/*void horse::display_horse(float elapsed_time)
+{
+    set_cursor_pos(1 + int(distance_run * CPF), 2 + horse_number * 2);
+    set_color(static_cast<color>(cBLUE + horse_number));
+    char horse_char = '0' + static_cast<char>(horse_number);
+    putch(' '); putch('\xDB'); putch(horse_char); putch('\xDB');
+    if(distance_run < ptrTrack->get_track_len() + 1.0 / CPF) {
+        if(rand() % 3)
+            distance_run += 0.2F;
+        finish_time = elapsed_time;
+    } else {
+        int mins = int(finish_time) / 60;
+        int secs = int(finish_time) – mins * 60;
+        cout << " Время =" << mins << ":" << secs;
+    }
+}*/
+track::track(float lenT, int nH) : track_length(lenT), total_horses(nH), horse_count(0), elapsed_time(0.0) {
+    // init_graphics();
+    total_horses = (total_horses > maxHorses) ? maxHorses : total_horses;
+    for(int j = 0; j < total_horses; j++)
+        hArray[j] = new horse(horse_count++, this);
+    time_t aTime;
+    srand(static_cast<unsigned>(time(&aTime)));
+    display_track();
+}
+track::~track() {
+    for(int j = 0; j < total_horses; j++)
+        delete hArray[j];   // уничтожить объекты класса horse, образованные конструктором класса track
+
+    // delete[] hArray;     // Эта запись удалит массив указателей, но не объекты
+}
+void track::display_track() {
+    //clear_screen();
+    for(int f = 0; f <= track_length; f++)
+        for(int r = 1; r <= total_horses * 2 + 1; r++) {
+            //set_cursor_pos(f * CPF + 5, r);
+            if(f == 0 || f == track_length)
+                cout << '\xDE';
+            else
+                cout << '\xB3';
+        }
+}
+void track::run() {
+    /*while(!kbhit()) {
+        elapsed_time += 1.75;
+        for(int j = 0; j < total_horses; j++)
+            hArray[j]->display_horse(elapsed_time);
+        wait(500);
+    }   
+    getch();
+    cout << endl; */
+}
+float track::get_track_len() const
+{ return track_length; }
+
 ///////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) 
@@ -250,6 +416,8 @@ int main(int argc, char* argv[])
     for(int j = 0; j < N; j++)
         cout << arrSort[j] << " ";
     cout << endl;
+
+/*
 
     //////////////////////////////////////////////////////////////
 
@@ -365,7 +533,36 @@ int main(int argc, char* argv[])
         newPersPtr[j]->printName();
     cout << endl;
 
+*/
+
     //////////////////////////////////////////////////////////////
+
+    char ans;           // 'д' или 'н'
+    char string[LEN];   // строка для разбора
+    cout << "\nВведите арифметическое выражение в виде 2+3*4/3-2"
+            "\nЧисла должны быть из одной цифры"
+            "\nНе используйте пробелы и скобки";
+    do {
+        cout << "\nВыражение: ";
+        cin >> string;                              // вводим строку
+        express* eptr = new express(string);        // создаем объект для разбора
+        eptr->parse();      // разбираем
+        cout << "\nРезультат: " << eptr->solve() << endl;   // решаем
+        delete eptr;        // удаляем объект
+        cout << "Еще одно выражение (д/н)? ";
+        cin >> ans;
+    } while(ans == 'д');
+
+    ///////////////////////////////////////////////////////////
+
+    float length;
+    int total;
+    cout << "\nВведите длину дистанции (от 1 до 12): ";
+    cin >> length;
+    cout << "\nВведите количество лошадей (от 1 до 7): ";
+    cin >> total;
+    track theTrack(length, total);
+    theTrack.run();
 
     return 0;
 }
